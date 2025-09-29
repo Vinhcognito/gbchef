42a0cefffbda4090d05af6ad77202aae02c0d1b3
E. Maric <emaric.dev@gmail.com>
2025-09-29 18:20:27 +0800

    :sparkle: :lipstick: Implemented Drag Selection of Ingredients

--- gbchef/Controls/SelectableListBox.xaml.cs
+++ gbchef/Controls/SelectableListBox.xaml.cs
@@ -3,6 +3,7 @@ using gbchef.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,11 @@ namespace gbchef.Controls
 {
     public partial class SelectableListBox : UserControl
     {
+        private bool _selectionState = false;
+        private bool _isDragging;
+        private Point _startPosition;
+        private readonly HashSet<SelectableIngredient> _processedItems = new();
+
         public static readonly DependencyProperty ItemsProperty =
             DependencyProperty.Register(nameof(Items), typeof(ObservableCollection<SelectableIngredient>),
                 typeof(SelectableListBox), new PropertyMetadata(null));
@@ -37,19 +43,8 @@ namespace gbchef.Controls
 
         }
 
-        private void ListBox_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            var listBox = (ListBox)sender;
-            var item = listBox.SelectedItem as SelectableIngredient;
-
-            if (item != null) {
-                item.IsSelected = !item.IsSelected;
-            }
-        }
-
         private void ClearButton_ClickHandler(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("ClearButton click");
             foreach (SelectableIngredient ingredient in Items)
             {
                 ingredient.IsSelected = false;
@@ -58,11 +53,63 @@ namespace gbchef.Controls
 
         private void SelectAllButton_ClickHandler(object sender, RoutedEventArgs e)
         {
-            Console.WriteLine("Select All click");
             foreach (SelectableIngredient ingredient in Items)
             {
                 ingredient.IsSelected = true;
             }
+        }
+
+        private void InnerListBox_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var listBox = (ListBox)sender;
+            var item = FindItemUnderMouse(listBox, e);
+            
+            if (item != null)
+            {
+                _isDragging = true;
+                _selectionState = !item.IsSelected;
+                _startPosition = e.GetPosition(listBox);
+                _processedItems.Clear();
+
+                item.IsSelected = _selectionState;
+                _processedItems.Add(item);
+                e.Handled = true;
+            }
+        }
+
+        private void InnerListBox_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_isDragging || e.LeftButton != MouseButtonState.Pressed) return;
+
+            var listBox = (ListBox)sender;
+            var currentPosition = e.GetPosition(listBox);
+
+            if (Math.Abs(currentPosition.Y - _startPosition.Y) < SystemParameters.MinimumVerticalDragDistance) return;
+
+            var item = FindItemUnderMouse(listBox, e);
+            if (item != null && !_processedItems.Contains(item))
+            {
+                item.IsSelected = _selectionState;
+                _processedItems.Add(item);
+            }
+        }
+
+        private void InnerListBox_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isDragging = false;
+            _processedItems.Clear();
+        }
+
+        private SelectableIngredient? FindItemUnderMouse(ListBox listBox, MouseEventArgs e)
+        {
+            var visual = listBox.InputHitTest(e.GetPosition(listBox)) as DependencyObject;
+
+            while (visual != null && visual is not ListBoxItem)
+            {
+                visual = VisualTreeHelper.GetParent(visual);
+            }
+
+            return (visual as ListBoxItem)?.Content as SelectableIngredient;
         }
 
     }
